9deb5007cb2d8999ed4ed1d34849a90c4b7cce95
Brock <Lynch.b@hotmail.com>
2022-07-19 16:56:51 -0400

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,14 +1,53 @@
-function getValue(message){
-    alert("HELLO WOLRD!");
+function getValue(){
+    let message = document.getElementById("message").value;
+    let revMessage = reverse(message);
+    let pallendrome = pallendromeCheck(message, revMessage);
+    displayReversed(revMessage, pallendrome);
 }
-function getValue2(){
-    let msg = '';
-    msg = document.getElementById("message").value;
-    swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+
+// bussness logic
+function reverse(message){
+    let messageArr = [];
+    for(let i=message.length-1;i>=0;i--){
+        messageArr+=message[i];
+    }
+    return messageArr.toString();
+}
+
+
+function pallendromeCheck(message,reverse){
+    message = message.toUpperCase();
+    reverse = reverse.toUpperCase();
+    if(message == reverse){
+        return true;
+    }
+    return false;
+    // let stop = Math.ceil(message.length/2);
+    // for(let i=0;i<stop;i++){
+    //     let reverse = message.length-1-i;
+    //     if(message[i]!=message[reverse]){
+    //         return false;
+    //     }
+    // }
+    // return true;
+}
+
+// display the reversed string
+function displayReversed(revMessage, pallendrome){
+    let results = document.getElementById("results");
+    results.innerHTML = "";
+    let div = document.createElement("div"); 
+    results.classList.add("revMessage");
+    if(pallendrome==true){
+        results.innerHTML = revMessage 
+        div.innerHTML = " This is Tacocat approved!!";
+        results.appendChild(div);
+    }else{
+        results.innerHTML = revMessage;
+        div.innerHTML = "This in NOT Tococat approved";
+        results.appendChild(div);
+    }
+    
+
+
 }
